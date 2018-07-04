55736f65f305ab27ff44ad694a2a7ce93f3d9acf
jenkl <jenkl@yandex-team.ru>
2018-07-04 17:59:07 +0300

    initial commit

---
+++ src/test/java/ru/jenkl/SimpleTest.java
@@ -0,0 +1,30 @@
+package ru.jenkl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Anton Tsyganov (jenkl)
+ * @date 04.07.18
+ */
+public class SimpleTest {
+    @Test
+    public void shouldFinishedSuccess() {
+
+    }
+
+    @Test
+    public void shouldFinishedSuccess2() {
+
+    }
+
+    @Test
+    public void shouldBeFailed() {
+        Assert.fail();
+    }
+
+    @Test
+    public void shouldBeBroken() {
+        throw new RuntimeException();
+    }
+}
