e5572d8a0b7b470347a41f467e0ae43fa99a5a56
jenkl <jenkl@yandex-team.ru>
2018-07-17 12:40:30 +0300

    add sout

--- src/test/java/ru/jenkl/RandomFailedTest.java
+++ src/test/java/ru/jenkl/RandomFailedTest.java
@@ -14,12 +14,14 @@ public class RandomFailedTest {
     @Test
     public void testRandom() {
         int i = new Random().nextInt(100);
+        System.out.println("*** random i in testRandom is " + i);
         Assert.assertTrue(i + " is not even", i%2 == 0);
     }
 
     @Test
     public void testRandom2() {
         int i = new Random().nextInt(100);
+        System.out.println("*** random i in testRandom2 is " + i);
         Assert.assertTrue(i + " is even",i%2 != 0);
     }
 }
