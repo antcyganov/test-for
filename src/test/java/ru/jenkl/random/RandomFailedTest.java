cc558e8e7ed5cc1dd9ebd604f4e9f43650a0e09c
jenkl <jenkl@yandex-team.ru>
2018-09-11 17:01:22 +0300

    new branch

--- src/test/java/ru/jenkl/random/RandomFailedTest.java
+++ src/test/java/ru/jenkl/random/RandomFailedTest.java
@@ -1,7 +1,6 @@
 package ru.jenkl.random;
 
 import com.google.code.tempusfugit.concurrency.IntermittentTestRunner;
-import com.google.code.tempusfugit.concurrency.annotations.Intermittent;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +11,6 @@ import java.util.Random;
  * @author Anton Tsyganov (jenkl)
  * @date 17.07.18
  */
-@Intermittent
 @RunWith(IntermittentTestRunner.class)
 public class RandomFailedTest {
 
